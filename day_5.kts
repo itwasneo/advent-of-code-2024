3e880882b0ad86e20363a6ce2a6a451aa631bf4c
hokomoko <kaan.yildiz@protonmail.com>
2024-12-05 14:05:22 +0300

    Day 5 - refactored placeNode function to make it more clear, added some comments

--- day_5.kts
+++ day_5.kts
@@ -4,8 +4,115 @@ import kotlin.time.measureTime
 
 var r: HashMap<Int, MutableSet<Int>> = HashMap()
 var u: MutableList<MutableList<Int>> = mutableListOf()
-var u2: MutableList<MutableList<Int>> = mutableListOf() // The second part's input
+var u2: MutableList<MutableList<Int>> =
+    mutableListOf() // The second part's input
 
+/**
+ * Used a "seen" set.
+ * Whenever a "seen" value, appeared in a rule set, that update line become invalid
+ */
+fun solve1() {
+    val result = u.sumOf { update ->
+        val seen = mutableSetOf<Int>()
+        var valid = true
+        for (cu in update) {
+            if (!r[cu].isNullOrEmpty() && r[cu]!!.any { seen.contains(it) }) {
+                valid = false
+                u2.add(update) // This prepares the input for the second part :)
+                break
+            }
+            seen.add(cu)
+        }
+        if (valid) update[update.size / 2] else 0
+    }
+    println("1: $result")
+}
+
+data class Node(var v: Int, var next: Node?)
+
+data class LinkedList(var head: Node?)
+
+/**
+ * Used a linked list to sort the updates.
+ */
+fun solve2() {
+    val result = u2.sumOf { update ->
+        val ll = LinkedList(null)
+        for (n in update) {
+            placeNode(n, ll)
+        }
+        getAt(ll.head, update.size / 2)
+    }
+
+    println("2: $result")
+}
+
+
+/**
+ * Given a linked list and a value, it finds the right spot to insert the new node
+ * (according to the rule set)
+ */
+fun placeNode(c: Int, ll: LinkedList) {
+    val rules = r[c] ?: emptySet<Int>()
+    if (ll.head == null || rules.contains(ll.head!!.v)) { // Insert head if null, or has priority
+        ll.head = Node(c, ll.head)
+        return
+    }
+    var cur = ll.head
+    while (cur != null) {
+        when {
+            cur.next == null -> {
+                cur.next = Node(c, null)
+                return
+            }
+
+            rules.contains(cur.next!!.v) -> {
+                cur.next = Node(c, cur.next)
+                return
+            }
+
+            else -> cur = cur.next
+        }
+    }
+}
+
+/**
+ * Debugging utility for printing nodes in a linked list
+ */
+fun printLinkedList(node: Node?) {
+    if (node != null) {
+        if (node.next == null) print("${node.v} --> ")
+        else print("${node.v} --> ")
+        printLinkedList(node.next)
+    }
+}
+
+/**
+ * Get the specific node value at an index
+ */
+fun getAt(node: Node?, at: Int): Int {
+    var cur = node
+    repeat(at) {
+        cur = cur!!.next
+    }
+    return cur!!.v
+}
+
+val p = measureTime {
+    readFile()
+}
+
+val t = measureTime {
+    solve1()
+    solve2()
+}
+
+println("P: $p")
+println("T: $t")
+
+/**
+ * Yes I parsed like this
+ */
 fun readFile() {
     var s = true
     Files.readAllLines(Path("input\\day5_input.txt")).forEach { l ->
@@ -28,86 +135,3 @@ fun readFile() {
     }
 }
 
-fun solve1() {
-    val result = u.sumOf { update ->
-        val seen = mutableSetOf<Int>()
-        var valid = true
-        for (cu in update) {
-            if (!r[cu].isNullOrEmpty() && r[cu]!!.any { seen.contains(it) }) {
-                valid = false
-                u2.add(update) // This prepares the input for the second part :)
-                break
-            }
-            seen.add(cu)
-        }
-        if (valid) update[update.size / 2] else 0
-    }
-    println("1: $result")
-}
-
-data class Node(var v: Int, var next: Node?)
-
-data class LinkedList(var head: Node?)
-
-fun solve2() {
-    val result = u2.sumOf { update ->
-        val ll = LinkedList(null)
-        for (n in update) {
-            placeNode(n, ll)
-        }
-        getMid(ll.head, update.size/2)
-    }
-
-    println("2: $result")
-}
-
-fun placeNode(c: Int, ll: LinkedList) {
-    if (ll.head == null) {
-        ll.head = Node(c, null)
-    } else {
-        if (r[c] != null && r[c]!!.contains(ll.head!!.v)) {
-            val newHead = Node(c, ll.head)
-            ll.head = newHead
-        } else {
-            var cur = ll.head
-            while (cur != null) {
-                if (cur.next == null) {
-                    cur.next = Node(c, null)
-                    break
-                } else if (r[c] != null && r[c]!!.contains(cur.next!!.v)) {
-                    cur.next = Node(c, cur.next)
-                    break
-                } else {
-                    cur = cur.next
-                }
-            }
-        }
-    }
-}
-
-fun printLinkedList(node: Node?) {
-    if (node != null) {
-        print("${node.v} --> ")
-        printLinkedList(node.next)
-    }
-}
-
-fun getMid(node: Node?, mid: Int): Int {
-    var cur = node
-    repeat(mid) {
-        cur = cur!!.next
-    }
-    return cur!!.v
-}
-
-val p = measureTime {
-    readFile()
-}
-
-val t = measureTime {
-    solve1()
-    solve2()
-}
-
-println("P: $p")
-println("T: $t")
