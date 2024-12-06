5119881b6ad84021602092e665da3fb83a9caa6d
hokomoko <kaan.yildiz@protonmail.com>
2024-12-06 15:14:20 +0300

    Day6 little cleanup, reduced elapsed time with better set

--- day_6.kts
+++ day_6.kts
@@ -58,20 +58,18 @@ fun solve1() {
     println("1: $result")
 }
 
-fun checkCyclic(cx: Int, cy: Int): Boolean {
+fun checkCyclic(): Boolean {
     var x = st!!.first
     var y = st!!.second
     var dir = Pair(0, -1)
-    val edges = mutableSetOf<Pair<String, String>>()
+    val edges = mutableSetOf<Pair<Int, Int>>()
     while (x in om.indices && y in om[0].indices) {
-        while (om[y][x]) {
+        if (om[y][x]) {
             x -= dir.first
             y -= dir.second
             dir = dir.next()
-            x += dir.first
-            y += dir.second
         }
-        val e = Pair("${x - dir.first}-${y - dir.second}", "$x-$y")
+        val e = Pair(to1D(x - dir.first, y - dir.second), to1D(x, y))
         if (edges.contains(e)) return true
         edges.add(e)
         x += dir.first
@@ -80,11 +78,13 @@ fun checkCyclic(cx: Int, cy: Int): Boolean {
     return false
 }
 
+fun to1D(x: Int, y: Int): Int = x * size + y
+
 fun solve2() {
     var result = 0
     rm.forEach { p ->
         om[p.second][p.first] = true
-        if (checkCyclic(p.first, p.second)) {
+        if (checkCyclic()) {
             result++
         }
         om[p.second][p.first] = false
@@ -98,8 +98,12 @@ val p = measureTime {
 
 val t1 = measureTime {
     solve1()
+}
+
+val t2 = measureTime {
     solve2()
 }
 
 println("P: $p")
 println("T1: $t1")
+println("T2: $t2")
