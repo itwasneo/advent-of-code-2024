3ffa2e73c432ab0e5764ab48272d0a36e88ee9a2
hokomoko <kaan.yildiz@protonmail.com>
2024-12-07 12:32:01 +0300

    Day7 - using my own Linked List - massive performance gain

--- day_7.kts
+++ day_7.kts
@@ -16,11 +16,15 @@ fun readAndSolve() {
         while (!line.isNullOrEmpty()) {
             val (res, nums) = line.split(":", limit = 2).map(String::trim)
             val target = res.toLong()
-            val numbers = nums.split(" ").map(String::trim).map(String::toLong)
+            val ll = LinkedList(null)
+            nums.split(" ").map(String::trim).map(String::toLong)
+                .forEach { lvalue ->
+                    ll.add(lvalue)
+                }
 
             if (addMul(
-                    numbers.first(),
-                    numbers.drop(1),
+                    ll.head!!.value,
+                    ll.head!!.next,
                     target
                 )
             ) {
@@ -28,37 +32,50 @@ fun readAndSolve() {
                 part2 += target // Not sure whether they design the input that way but this may prevent to run modified recursion
             } // Part1 Calculation
             else if (addMulConcat(
-                    numbers.first(),
-                    numbers.drop(1),
+                    ll.head!!.value,
+                    ll.head!!.next,
                     target
                 )
             ) part2 += target // Part2 Calculation
             line = br.readLine()
         }
-
     }
     println("1: $part1")
     println("2: $part2")
 }
 
-fun addMul(acc: Long, i: List<Long>, t: Long): Boolean {
-    if (i.isNotEmpty()) {
-        val next = i.first()
-        return addMul(acc + next, i.drop(1), t) ||
-                addMul(acc * next, i.drop(1), t)
+fun addMul(acc: Long, node: Node?, t: Long): Boolean {
+    return if (node != null) {
+        addMul(acc + node.value, node.next, t) ||
+                addMul(acc * node.value, node.next, t)
     } else {
-        return acc == t
+        acc == t
     }
 }
 
-fun addMulConcat(acc: Long, i: List<Long>, t: Long): Boolean {
-    if (i.isNotEmpty()) {
-        val next = i.first()
-        return addMulConcat(acc + next, i.drop(1), t) ||
-                addMulConcat(acc * next, i.drop(1), t) ||
-                addMulConcat("$acc$next".toLong(), i.drop(1), t)
+fun addMulConcat(acc: Long, node: Node?, t: Long): Boolean {
+    return if (node != null) {
+        addMulConcat(acc + node.value, node.next, t) ||
+                addMulConcat(acc * node.value, node.next, t) ||
+                addMulConcat("$acc${node.value}".toLong(), node.next, t)
     } else {
-        return acc == t
+        acc == t
+    }
+}
+
+data class Node(val value: Long, var next: Node?)
+
+data class LinkedList(var head: Node?) {
+    fun add(value: Long) {
+        if (head == null) {
+            head = Node(value, null)
+        } else {
+            var curr = head!!
+            while (curr.next != null) {
+                curr = curr.next!!
+            }
+            curr.next = Node(value, null)
+        }
     }
 }
 
