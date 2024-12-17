b5c07892f504c7afb3ca170b305577e729413dd9
hokomoko <kaan.yildiz@protonmail.com>
2024-12-18 02:30:09 +0300

    Day17 - part1 and part2 - finalized

--- day_17.kts
+++ day_17.kts
@@ -2,16 +2,20 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.time.measureTime
 
+/**
+ * Very fun question :) neo approved
+ */
+
 // Registers
-var ra: Int? = null
-var rb: Int? = null
-var rc: Int? = null
+var ra: Long? = null
+var rb: Long? = null
+var rc: Long? = null
 
 // Instruction Pointer
 var ip: Int? = 0 // Increases by 2 after each instructions (opcode + operand)
 
 // Output
-var ou: String = ""
+var ou: MutableList<Int> = mutableListOf()
 
 // Program
 var pr: List<Int>? = null
@@ -21,11 +25,11 @@ enum class Operand {
     COMBO
 }
 
-fun getOperandValue(operand: Operand, id: Int): Int {
+fun getOperandValue(operand: Operand, id: Int): Long {
     return when (operand) {
-        Operand.LITERAL -> id
+        Operand.LITERAL -> id.toLong()
         Operand.COMBO -> when (id) {
-            0, 1, 2, 3 -> id
+            0, 1, 2, 3 -> id.toLong()
             4 -> ra!!
             5 -> rb!!
             6 -> rc!!
@@ -44,10 +48,11 @@ fun getOperandValue(operand: Operand, id: Int): Int {
  *  denominator:    operand(COMBO)
  */
 fun adv(operand: Int) {
-    ra = ra!! / (1 shl getOperandValue(
-        Operand.COMBO,
-        operand
-    )) // by default rounds to zero
+    ra =
+        ra!! shr getOperandValue( // dividing a number to power of 2 -> shifting right
+            Operand.COMBO,
+            operand
+        ).toInt() // by default rounds to zero
     jump()
 }
 
@@ -68,7 +73,10 @@ fun bxl(operand: Int) {
  * input:   operand(COMBO)
  */
 fun bst(operand: Int) {
-    rb = getOperandValue(Operand.COMBO, operand) % 8
+    rb = getOperandValue(
+        Operand.COMBO,
+        operand
+    ) and 7 // Taking last 3 bits by bitwise and with 7
     jump()
 }
 
@@ -80,8 +88,8 @@ fun bst(operand: Int) {
  * input: operand(LITERAL)
  */
 fun jnz(operand: Int) {
-    if (ra!! != 0) ip =
-        getOperandValue(Operand.LITERAL, operand) else jump()
+    if (ra!! != 0L) ip =
+        getOperandValue(Operand.LITERAL, operand).toInt() else jump()
 }
 
 /**
@@ -90,7 +98,7 @@ fun jnz(operand: Int) {
  * left: rb
  * right: rc
  */
-fun bxc(operand: Int) {
+fun bxc() {
     rb = rb!! xor rc!!
     jump()
 }
@@ -100,9 +108,11 @@ fun bxc(operand: Int) {
  * opcode: 5
  * input: operand(COMBO)
  */
-fun out(operand: Int) {
-    ou += "${getOperandValue(Operand.COMBO, operand) % 8},"
+fun out(operand: Int): Int {
+    val result = getOperandValue(Operand.COMBO, operand) and 7
+    ou.add(result.toInt())
     jump()
+    return result.toInt()
 }
 
 /**
@@ -113,25 +123,27 @@ fun out(operand: Int) {
  *  denominator:    operand(COMBO)
  */
 fun bdv(operand: Int) {
-    rb = ra!! / (1 shl getOperandValue(
-        Operand.COMBO,
-        operand
-    )) // by default rounds to zero
+    rb =
+        ra!! shr getOperandValue( // dividing a number to power of 2 -> shifting right
+            Operand.COMBO,
+            operand
+        ).toInt() // by default rounds to zero
     jump()
 }
 
 /**
- *  bdv (division) truncates (rounding to zero) the result to an integer. Writes
+ *  cdv (division) truncates (rounding to zero) the result to an integer. Writes
  *  the result into register C
  *  opcode: 7
  *  numerator:      ra
  *  denominator:    operand(COMBO)
  */
 fun cdv(operand: Int) {
-    rc = ra!! / (1 shl getOperandValue(
-        Operand.COMBO,
-        operand
-    )) // by default rounds to zero
+    rc =
+        ra!! shr getOperandValue( // dividing a number to power of 2 -> shifting right
+            Operand.COMBO,
+            operand
+        ).toInt() // by default rounds to zero
     jump()
 }
 
@@ -139,6 +151,10 @@ fun jump() {
     ip = ip!! + 2
 }
 
+/**
+ * Part 1 is pretty straightforward if you can manage to define the instructions
+ * correctly
+ */
 fun solve1() {
     while (ip in pr!!.indices) {
         val ins = pr!![ip!!]
@@ -148,23 +164,97 @@ fun solve1() {
             1 -> bxl(ope)
             2 -> bst(ope)
             3 -> jnz(ope)
-            4 -> bxc(ope)
+            4 -> bxc()
             5 -> out(ope)
             6 -> bdv(ope)
             7 -> cdv(ope)
             else -> throw RuntimeException("This shouldn't happen")
         }
     }
-    println(ou)
+    println("1: ${ou.joinToString(",")}")
+    ip = 0
+    rb = 0
+    rc = 0
+    ou.clear()
+}
+
+/**
+ * This function runs the program just one time and returns the number in the output
+ */
+fun runProgramOneTime(): Int {
+    while (ip in pr!!.indices) {
+        val ins = pr!![ip!!]
+        val ope = pr!![ip!! + 1]
+        when (ins) {
+            0 -> adv(ope)
+            1 -> bxl(ope)
+            2 -> bst(ope)
+            3 -> jnz(ope)
+            4 -> bxc()
+            5 -> out(ope)
+            6 -> bdv(ope)
+            7 -> cdv(ope)
+            else -> throw RuntimeException("This shouldn't happen")
+        }
+    }
+    val result = ou[0]
+    ip = 0
+    rb = 0
+    rc = 0
+    ou.clear()
+    return result
+}
+
+/**
+ * Part 2 was very fun to implement after realizing the pattern of the program.
+ * Program outputs one number at each iteration. And at each iteration value at
+ * register A shifts right 3 times(gets divided by 8). So we can conclude that
+ * at the last iteration, program should output 0. Since everything starts with
+ * register A, when we find 3 bits that makes the program result in 0, we end up
+ * finding the FIRST 3 bits of expected register A value at the end. Running the
+ * program for each value in the program code reverse, while updating the register
+ * A value gives the target.
+ */
+fun solve2() {
+    val targets = pr!!.reversed()
+    var a = 0L
+    var cur = 0 // current target index, that program should output
+    var skip = 0 // !!crucial for recalculation when no matching found
+    while (cur in targets.indices) {
+        val target = targets[cur]
+        var success = false
+        for (i in 0 until 8) {
+            ra = a + i
+            val programResult = runProgramOneTime()
+            if (target == programResult) {
+                if (skip == 0) {
+                    a += i
+                    success = true
+                    break
+                } else {
+                    skip--
+                }
+            }
+        }
+        if (success) {
+            cur++
+            a = a shl 3 // Shift left and append bits
+        } else { // If no 3 bit found, we need to go back and find the next smallest alternative for the PREVIOUS 3 bits
+            cur--
+            skip++ // This will help skipping the previously used alternatives
+            a = a shr 3
+        }
+    }
+    println(a shr 3)
 }
 
 
 fun readInput() {
-    val (registers, instructions) = Files.readString(Paths.get("input\\day17_input_example.txt"))
+    val (registers, instructions) = Files.readString(Paths.get("input\\day17_input.txt"))
         .split("\r\n\r\n".toRegex(), limit = 2)
 
-    fun gR(l: String): Int {
-        return l.split(":", limit = 2)[1].trim().toInt()
+    fun gR(l: String): Long {
+        return l.split(":", limit = 2)[1].trim().toLong()
     }
 
     registers.lines().forEachIndexed { i, l ->
@@ -176,13 +266,6 @@ fun readInput() {
     }
     pr = instructions.split(":", limit = 2)[1].split(",").map(String::trim)
         .map(String::toInt)
-
-    /*
-    println("ra: $ra")
-    println("rb: $rb")
-    println("rc: $rc")
-    println("Instructions: $pr")
-     */
 }
 
 val p = measureTime {
@@ -194,3 +277,9 @@ val t1 = measureTime {
     solve1()
 }
 println("T1: $t1")
+
+
+val t2 = measureTime {
+    solve2()
+}
+println("T2: $t2")
