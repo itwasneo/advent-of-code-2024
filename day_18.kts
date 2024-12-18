58296e64c687389e740140a431a8eae3eb87c549
hokomoko <kaan.yildiz@protonmail.com>
2024-12-18 19:56:47 +0300

    Day18 - added explanation - finalized

--- day_18.kts
+++ day_18.kts
@@ -7,7 +7,8 @@ val size = 71
 val grid = Array(size) { BooleanArray(size) { false } }
 var start = Vec2(0, 0)
 var exit = Vec2(size - 1, size - 1)
-var nodes = mutableListOf<Vec2>()
+var nodes = mutableSetOf<Vec2>()
+var input: List<String>? = null
 
 // Direction utilities
 val right = Vec2(1, 0)
@@ -16,45 +17,35 @@ val left = Vec2(-1, 0)
 val up = Vec2(0, -1)
 val dirs = listOf(right, down, left, up)
 
-
-// Type represents a single node on a shortest path graph (Direction matters)
-//typealias Node = Pair<Vec2, Vec2>
-
-//data class Node(val xy: Vec2, val d: Vec2)
-
-// Start is top left = 0, 0
-// Exit is bottom right = size - 1, size - 1
-
-fun readInput(firstN: Int) {
-    Files.readAllLines(Paths.get("input\\day18_input.txt")).take(firstN)
-        .forEach { xy ->
-            val v = xy.split(",", limit = 2).map(String::toInt)
-            grid[v[1]][v[0]] = true
-        }
+/**
+ * Very easy puzzle for day 18 :( A classic shortest path. For part 2 I just add
+ * new corrupted locations to the grid and remove that location from nodes and
+ * run the shortest path algorithm again. First corrupted location that makes the
+ * exit distance to Int.MAX_VALUE is the solution.
+ */
+fun solve1(firstN: Int) {
+    input = Files.readAllLines(Paths.get("input\\day18_input.txt"))
+    input!!.take(firstN).forEach { xy ->
+        val v = xy.split(",", limit = 2).map(String::toInt)
+        grid[v[1]][v[0]] = true
+    }
 
     grid.forEachIndexed { y, r ->
         r.forEachIndexed { x, c ->
-            if (c) {
-                //print("#")
-            } else {
+            if (!c) {
                 nodes.add(Vec2(x, y))
-                //print(".")
             }
         }
-        //print("\n")
     }
+    println("1: ${shortestPath()[exit]}")
 }
 
-fun solve1(firstN: Int) {
-    readInput(firstN)
-    val distances = shortestPath()
-    println(distances[exit])
-}
-
+/**
+ * Classic Dijkstra (with graph determined while running)
+ */
 fun shortestPath(): Map<Vec2, Int> {
     val distances = mutableMapOf<Vec2, Int>()
 
-    // Initializing each distance with MAX int value
     nodes.forEach { n ->
         distances[n] = Int.MAX_VALUE
     }
@@ -67,8 +58,7 @@ fun shortestPath(): Map<Vec2, Int> {
 
         if (cd > distances[cn]!!) continue
 
-        val adjs = getAdjNodes(cn)
-        adjs.forEach { neighbor ->
+        getAdjNodes(cn).forEach { neighbor ->
             val newDistance = cd + 1
             if (newDistance < distances[neighbor]!!) {
                 distances[neighbor] = newDistance
@@ -80,6 +70,9 @@ fun shortestPath(): Map<Vec2, Int> {
     return distances
 }
 
+/**
+ * Returns the current node's neighbors (this could have been pre-calculated)
+ */
 fun getAdjNodes(node: Vec2): List<Vec2> {
     return dirs.filter { d ->
         val x = node.x + d.x
@@ -89,28 +82,19 @@ fun getAdjNodes(node: Vec2): List<Vec2> {
 }
 
 fun solve2(firstN: Int) {
-    val reduced =
-        Files.readAllLines(Paths.get("input\\day18_input.txt")).drop(firstN)
+    val reduced = input!!.drop(firstN)
     var idx = 0
     for (xy in reduced) {
         val v = xy.split(",", limit = 2).map(String::toInt)
         grid[v[1]][v[0]] = true
         nodes.remove(Vec2(v[0], v[1]))
-        val distances = shortestPath()
-        if (distances[exit] == Int.MAX_VALUE) {
+        if (shortestPath()[exit] == Int.MAX_VALUE) {
             println("2: ${reduced[idx]}")
             break
         }
         idx++
     }
 }
-
-/*
-val p = measureTime {
-    readInput()
-}
-println("P: $p")
- */
 
 val t1 = measureTime {
     solve1(1024)
